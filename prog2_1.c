2c5294b528b0e599ce524ed736af7efc4ee9767e
Chris Gilardi <cgilardi97@gmail.com>
2018-12-10 15:04:18 -0800

    Linked List working with mpz_t type

--- prog2_1.c
+++ prog2_1.c
@@ -7,74 +7,86 @@ TSAFELIST* tSafeConstruct() {
     TSAFELIST* list = (TSAFELIST*) malloc(sizeof(TSAFELIST));
     list->head = NULL;
     list->mutex = malloc(sizeof(pthread_mutex_t));
+    pthread_mutex_init(list->mutex, NULL);
     return list;
 }
 
 void tSafeDestruct(TSAFELIST* list) {
-  // for each node in the list, free all nodes,
-  if(list == NULL) {
-    return;
-  }
-
-  TSAFENODE* node = list->head;
-  TSAFENODE* temp;
-  int i = 0;
-
-  while(node != NULL) {
-    temp = node;
-    node = node->next;
-    free(temp);
-    //printf("freed entry %d\n", i);
-    i++;
-  }
-  //then:
-  free(list);
+    pthread_mutex_lock(list->mutex);
+    // for each node in the list, free all nodes,
+    if(list == NULL) {
+        return;
+    }
+    
+    TSAFENODE* node = list->head;
+    TSAFENODE* temp;
+    int i = 0;
+    
+    while(node != NULL) {
+        temp = node;
+        node = node->next;
+        free(temp);
+        //printf("freed entry %d\n", i);
+        i++;
+    }
+    pthread_mutex_unlock(list->mutex);
+    //then:
+    free(list->mutex);
+    free(list);
 }
 
 void tSafeEnqueue(TSAFELIST * list, mpz_t item) {
-  if(list == NULL) {
-    //printf("list null\n");
-    // don't do things
-  }
-  TSAFENODE* node;
-  if(list->head == NULL) {
-    //printf("list empty\n");
-    list->head = (TSAFENODE*) malloc(sizeof(TSAFENODE));
-    node = list->head;
-    //node->number = item;
-    node->next = NULL;
-    //printf("added: %d\n", list->head->number);
-  } else {
-    int i = 0;
-    TSAFENODE* toAdd = malloc(sizeof(TSAFENODE));
-    node = list->head;
-    //toAdd->number = item;
-    toAdd->next = NULL;
-    // walk the list
-    while(node->next != NULL) {
-      //printf("%d: %d\n", i, node->number);fflush(NULL);
-      node = node->next;
-      i++;
+    pthread_mutex_lock(list->mutex);
+//    gmp_printf ("item value: %Zd\n", item);
+    if(list == NULL) {
+        //printf("list null\n");
+        // don't do things
+        return;
     }
-
-    node->next = toAdd;
-    //printf("%d: %d\n", i, node->number);fflush(NULL);
-    //printf("added (%d): %d\n", ++i, node->next->number);
-  }
-
+    TSAFENODE* node;
+    if(list->head == NULL) {
+        //printf("list empty\n");
+        list->head = (TSAFENODE*) malloc(sizeof(TSAFENODE));
+        node = list->head;
+        mpz_init_set(node->number, item);
+//        gmp_printf ("node-num value: %Zd\n", node->number);
+        node->next = NULL;
+        //printf("added: %d\n", list->head->number);
+    } else {
+        int i = 0;
+        TSAFENODE* toAdd = malloc(sizeof(TSAFENODE));
+        node = list->head;
+        mpz_init_set(toAdd->number, item);
+//        gmp_printf ("toAdd value: %Zd\n", toAdd->number);
+        toAdd->next = NULL;
+        // walk the list
+        while(node->next != NULL) {
+            //printf("%d: %d\n", i, node->number);fflush(NULL);
+            node = node->next;
+            i++;
+        }
+        
+        node->next = toAdd;
+        //printf("%d: %d\n", i, node->number);fflush(NULL);
+        //printf("added (%d): %d\n", ++i, node->next->number);
+    }
+    pthread_mutex_unlock(list->mutex);
 }
 
 TSAFEDATA tSafeDequeue(TSAFELIST * list) {
-  TSAFEDATA data;
-  if(list == NULL) {
-    data.isValid = 0;
-    //data.value = 0;
-  }
-  TSAFENODE* tmpHead = list->head;
-  data.isValid = 1;
-  //data.value = tmpHead->number;
-
-  list->head = tmpHead->next;
-
-  return data;
+    pthread_mutex_lock(list->mutex);
+    TSAFEDATA data;
+    if(list == NULL) {
+        data.isValid = 0;
+        //data.value = 0;
+    }
+    TSAFENODE* tmpHead = list->head;
+    data.isValid = 1;
+    mpz_init_set(data.value, tmpHead->number);
+    
+//    gmp_printf ("data.value: %Zd\n", data.value);
+    
+    list->head = tmpHead->next;
+    pthread_mutex_unlock(list->mutex);
+    return data;
 }
--- prog2_2.c
+++ prog2_2.c
@@ -6,28 +6,41 @@
 
 
 mpz_t globalCounter;
+int foundCount = 0;
 pthread_mutex_t counterguard = PTHREAD_MUTEX_INITIALIZER;
 TSAFELIST *numberList;
 
-int main(int argc, char** argv) {
-  numberList = tSafeConstruct();
-  mpz_init_set_ui(globalCounter, 1);
-}
-
 void getNextNum(mpz_t * num) {
-  pthread_mutex_lock(&counterguard);
-  mpz_add_ui(globalCounter, globalCounter, 2);
-  mpz_set(*num, globalCounter);
-  pthread_mutex_unlock(&counterguard);
+    pthread_mutex_lock(&counterguard);
+    mpz_add_ui(globalCounter, globalCounter, 2);
+    //mpz_set(*num, globalCounter);
+    pthread_mutex_unlock(&counterguard);
 }
 
 void * generatePrimes(void * args) {
-  int prob = 0;
-  int i = 0;
-  mpz_t tempCounter;
-  mpz_init_set(tempCounter, globalCounter);
-  while(i < 100000) {
-    getNextNum(&tempCounter);
-    prob = mpz_probab_prime_p(tempCounter, 6);
-  }
+    int prob = 0;
+    int i = 0;
+    mpz_t tempCounter;
+    pthread_mutex_lock(&counterguard);
+    mpz_init_set(tempCounter, globalCounter);
+    pthread_mutex_unlock(&counterguard);
+    while(1) {
+        pthread_mutex_lock(&counterguard);
+        if(foundCount == 10) {
+            pthread_mutex_unlock(&counterguard);
+            return NULL;
+        }
+        pthread_mutex_unlock(&counterguard);
+        getNextNum(&tempCounter);
+        prob = mpz_probab_prime_p(tempCounter, 100000);
+        if(prob == 2) {
+            tSafeEnqueue(numberList, tempCounter);
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    numberList = tSafeConstruct();
+    mpz_init_set_ui(globalCounter, 1);
+    tSafeDestruct(numberList);
 }
